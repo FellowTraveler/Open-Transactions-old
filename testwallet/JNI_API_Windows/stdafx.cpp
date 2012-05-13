6b97418d27aceb622260f1c0f3933267c99ce7d4
Cameron Garnham <da2ce7@gmail.com>
2012-05-13 21:57:48 +1000

    rename folders

--- testwallet/JNI_API_Windows/stdafx.cpp
+++
@@ -1,6 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// JNI_API_Windows.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
