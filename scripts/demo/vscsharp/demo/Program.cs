16742ce9959554bbd01160d3f2f595d0d556f6e3
Cameron Garnham <da2ce7@gmail.com>
2013-04-02 14:47:27 +1100

    demo csharp app

---
+++ scripts/demo/vscsharp/demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = otvs.Load.Instance;
+        }
+    }
+}
