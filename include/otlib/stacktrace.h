4d287f266a62c1285f1aec149954140f7e7bc096
FellowTraveler <F3llowTraveler@gmail.com>
2013-12-07 02:43:01 -0600

    Added Android support to stacktrace.
    Signed-off-by: FellowTraveler <F3llowTraveler@gmail.com>

--- include/otlib/stacktrace.h
+++ include/otlib/stacktrace.h
@@ -7,16 +7,29 @@
 #include <cstdio>
 #include <cstdlib>
 
-#ifndef _WIN32
+// -----------------------------------------------------
+#ifndef _WIN32 // Not Windows
+// ------------------------
+#ifdef ANDROID
+#include <utils/CallStack.h>
+// ------------------------
+#else // not Android
 #include <execinfo.h>
 #include <cxxabi.h>
-#endif
+#endif // not Android
+// ------------------------
+#endif // Not Windows
+// -----------------------------------------------------
 
 /** Print a demangled stack backtrace of the caller function to FILE* out. */
 static inline void print_stacktrace(FILE *out = stderr, unsigned int max_frames = 63)
 {
 #ifdef _WIN32
 	//TODO: Write Winodws Code
+    
+#elif defined(ANDROID)
+    CallStack cs;
+    cs.dump();
 #else
 
     fprintf(out, "stack trace:\n");
@@ -44,53 +57,53 @@ static inline void print_stacktrace(FILE *out = stderr, unsigned int max_frames 
     // address of this function.
     for (int i = 1; i < addrlen; i++)
     {
-	char *begin_name = 0, *begin_offset = 0, *end_offset = 0;
+        char *begin_name = 0, *begin_offset = 0, *end_offset = 0;
 
-	// find parentheses and +address offset surrounding the mangled name:
-	// ./module(function+0x15c) [0x8048a6d]
-	for (char *p = symbollist[i]; *p; ++p)
-	{
-	    if (*p == '(')
-		begin_name = p;
-	    else if (*p == '+')
-		begin_offset = p;
-	    else if (*p == ')' && begin_offset) {
-		end_offset = p;
-		break;
-	    }
-	}
+        // find parentheses and +address offset surrounding the mangled name:
+        // ./module(function+0x15c) [0x8048a6d]
+        for (char *p = symbollist[i]; *p; ++p)
+        {
+            if (*p == '(')
+            begin_name = p;
+            else if (*p == '+')
+            begin_offset = p;
+            else if (*p == ')' && begin_offset) {
+            end_offset = p;
+            break;
+            }
+        }
 
-	if (begin_name && begin_offset && end_offset
-	    && begin_name < begin_offset)
-	{
-	    *begin_name++ = '\0';
-	    *begin_offset++ = '\0';
-	    *end_offset = '\0';
+        if (begin_name && begin_offset && end_offset
+            && begin_name < begin_offset)
+        {
+            *begin_name++ = '\0';
+            *begin_offset++ = '\0';
+            *end_offset = '\0';
 
-	    // mangled name is now in [begin_name, begin_offset) and caller
-	    // offset in [begin_offset, end_offset). now apply
-	    // __cxa_demangle():
+            // mangled name is now in [begin_name, begin_offset) and caller
+            // offset in [begin_offset, end_offset). now apply
+            // __cxa_demangle():
 
-	    int status;
-	    char* ret = abi::__cxa_demangle(begin_name,
-					    funcname, &funcnamesize, &status);
-	    if (status == 0) {
-		funcname = ret; // use possibly realloc()-ed string
-		fprintf(out, "  %s : %s+%s\n",
-			symbollist[i], funcname, begin_offset);
-	    }
-	    else {
-		// demangling failed. Output function name as a C function with
-		// no arguments.
-		fprintf(out, "  %s : %s()+%s\n",
-			symbollist[i], begin_name, begin_offset);
-	    }
-	}
-	else
-	{
-	    // couldn't parse the line? print the whole line.
-	    fprintf(out, "  %s\n", symbollist[i]);
-	}
+            int status;
+            char* ret = abi::__cxa_demangle(begin_name,
+                            funcname, &funcnamesize, &status);
+            if (status == 0) {
+            funcname = ret; // use possibly realloc()-ed string
+            fprintf(out, "  %s : %s+%s\n",
+                symbollist[i], funcname, begin_offset);
+            }
+            else {
+            // demangling failed. Output function name as a C function with
+            // no arguments.
+            fprintf(out, "  %s : %s()+%s\n",
+                symbollist[i], begin_name, begin_offset);
+            }
+        }
+        else
+        {
+            // couldn't parse the line? print the whole line.
+            fprintf(out, "  %s\n", symbollist[i]);
+        }
     }
 
     free(funcname);
